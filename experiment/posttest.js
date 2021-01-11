295d338d661d5a9e3f89030d93e6b98fc2cedce4
IITGVLAB <75976828+IITGVLAB@users.noreply.github.com>
2021-01-11 12:34:26 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -89,20 +89,22 @@
 
 
     const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1.How many basic binary subtraction operations are possible?:", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "1", ///// Write the option 1 inside double quotes
+                b: "4", ///// Write the option 2 inside double quotes
+                c: "3",
+                d: "2"
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "2. Why XOR gate is called an inverter?",  ///// Write the question inside double quotes
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
+        a: "Because of the same input",                  ///// Write the option 1 inside double quotes
+        b: "Because of the same output",                  ///// Write the option 2 inside double quotes
+        c: "It behaves like a NOT gate",      },
       correctAnswer: "c"                ///// Write the correct option inside double quotes
     },
 
